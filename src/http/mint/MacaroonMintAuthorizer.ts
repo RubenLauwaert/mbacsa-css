48db4af97a02032e8edeaddb48de39e776ee6be1
Ruben Lauwaert <rubenlauwaert1999@gmail.com>
2023-04-25 15:25:09 +0200

    Added Authentication / Authorization for Macaroon minting + added testfile

---
+++ src/http/mint/MacaroonMintAuthorizer.ts
@@ -0,0 +1,10 @@
+
+
+export class MacaroonMintAuthorizer {
+
+  public constructor(){}
+
+  
+
+
+}
--- src/http/mint/MacaroonMintHttpHandler.ts
+++ src/http/mint/MacaroonMintHttpHandler.ts
@@ -1,27 +1,59 @@
 import { ResponseDescription,
-  getLoggerFor, OperationHttpHandlerInput, OperationHttpHandler, guardedStreamFrom, OkResponseDescription, RepresentationMetadata } from '@solid/community-server';
+  getLoggerFor, OperationHttpHandlerInput, OperationHttpHandler, guardedStreamFrom, OkResponseDescription, RepresentationMetadata, CredentialsExtractor, ModesExtractor, PermissionReader, ensureTrailingSlash, CredentialSet, IdentifierSetMultiMap, AccessMode, ResourceIdentifier, Authorizer } from '@solid/community-server';
 import { MintRequestParser } from './MintRequestParser';
 import { MacaroonMinter } from './MacaroonMinter';
 
   export interface MacaroonMintHttpHandlerArgs {
-    endpointURI : string 
+    endpoint : string,
+    baseUrl : string,
+    /**
+     * Extracts the credentials from the incoming request.
+     */
+    credentialsExtractor: CredentialsExtractor;
+    /**
+     * Extracts the required modes from the generated Operation.
+     */
+    modesExtractor: ModesExtractor;
+    /**
+     * Reads the permissions available for the Operation.
+     */
+    permissionReader: PermissionReader; 
+    /**
+     * Verifies if the requested operation is allowed.
+     */
+    authorizer: Authorizer;
   }
 
 export class MacaroonMintHttpHandler extends OperationHttpHandler {
 private readonly logger = getLoggerFor(this);
-private readonly endpointURI : string;
+private readonly credentialsExtractor: CredentialsExtractor;
+private readonly modesExtractor: ModesExtractor;
+private readonly permissionReader: PermissionReader;
+private readonly authorizer: Authorizer;
+private readonly baseUrl : string;
+private readonly endpoint : string;
+private readonly mintUrl : string;
 
 public constructor(args: MacaroonMintHttpHandlerArgs) {
   super();
-  this.endpointURI = args.endpointURI;
+  this.baseUrl = ensureTrailingSlash(args.baseUrl);
+  this.endpoint = args.endpoint;
+  this.credentialsExtractor = args.credentialsExtractor;
+  this.modesExtractor = args.modesExtractor;
+  this.permissionReader = args.permissionReader;
+  this.authorizer = args.authorizer;
+  this.mintUrl = this.baseUrl + this.endpoint;
 }
 
 public async canHandle(input: OperationHttpHandlerInput): Promise<void> {
-  const { operation } = input;
-
-    if(!operation.target.path.includes(".macaroon/mint")){
+  const { operation, request } = input;
+    if(operation.target.path !== this.mintUrl){
       throw new Error();
     }
+
+
+
+
     // Check if body of request satisfies JSON Schema of MintRequest
     // if(operation.body.data.readable){
     //   const requestData = operation.body.data.read();
@@ -39,15 +71,51 @@ public async canHandle(input: OperationHttpHandlerInput): Promise<void> {
 
 public async handle(input: OperationHttpHandlerInput): Promise<ResponseDescription> {
   const { request, operation } = input;
-  // Parse Request data
-  const requestData = operation.body.data.read();
-  // Will throw an error if request dissatisfies JSON Schema
-  const mintRequest = MintRequestParser.parseMintRequest(requestData);
 
-  // Mint Macaroon 
-  const mintedMacaroon = await new MacaroonMinter().mintMacaroon(mintRequest);
-  const responseData = guardedStreamFrom(mintedMacaroon);
-  const response = new OkResponseDescription(new RepresentationMetadata(),responseData)
-  return response;
+  // Parse Request data
+  if(operation.body.data.readable){
+    const requestData = operation.body.data.read();
+    try {
+      // Will throw an error if request dissatisfies JSON Schema
+      const mintRequest = MintRequestParser.parseMintRequest(requestData);
+
+      // Authenticate requestor
+      const credentials: CredentialSet = await this.credentialsExtractor.handleSafe(request);
+      this.logger.info(`Extracted credentials: ${JSON.stringify(credentials)}`);
+      const {requestor} = mintRequest;
+      if(requestor !== credentials.agent!.webId){
+        throw new Error("Invalid credentials !");
+      }
+      // Authorize request
+      const {resourceURI} = mintRequest;
+      const requestedModes = new IdentifierSetMultiMap<AccessMode>()
+        .add({path: resourceURI},AccessMode.read);
+
+      const availablePermissions = await this.permissionReader.handleSafe({ credentials, requestedModes });
+      this.logger.info(`Available permissions are ${JSON.stringify(availablePermissions)}`);
+
+      try {
+        await this.authorizer.handleSafe({ credentials, requestedModes, availablePermissions });
+        operation.availablePermissions = availablePermissions;
+      } catch (error: unknown) {
+        this.logger.verbose(`Authorization failed: ${(error as any).message}`);
+        throw error;
+      }
+
+      this.logger.verbose(`Authorization succeeded !`);
+
+      // Mint Macaroon 
+      const mintedMacaroon = await new MacaroonMinter().mintMacaroon(mintRequest);
+      const responseData = guardedStreamFrom(mintedMacaroon);
+      const response = new OkResponseDescription(new RepresentationMetadata(),responseData)
+
+      return response;
+
+    } catch (error) {
+      throw new Error("Invalid JSON scheme for macaroon mint request !");
+    }
+  }else{
+    throw new Error("Invalid request body !");
+  }
 }
 }
